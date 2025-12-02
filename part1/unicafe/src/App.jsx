dc9eaa6efca98422e6d6cbc74265160e246495b4
Daniele Meca <daniele.meca@veroproject.local>
2025-12-02 12:28:06 +0100

    completed unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,51 +1,81 @@
+import { useState } from "react";
+
 export default function App() {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      { name: "Fundamentals of React", exercises: 10 },
-      { name: "Using props to pass data", exercises: 7 },
-      { name: "State of a component", exercises: 14 },
-    ],
-  };
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  function handleFeedback(type) {
+    setFeedback((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  }
 
   return (
-    <div>
-      <Header props={course.name} />
-      <Content props={course.parts} />
-      <Total props={course.parts} />
-    </div>
+    <>
+      <div>
+        <h1>give feedback</h1>
+        <Button text="good" value="good" onClick={handleFeedback}></Button>
+        <Button
+          text="neutral"
+          value="neutral"
+          onClick={handleFeedback}
+        ></Button>
+        <Button text="bad" value="bad" onClick={handleFeedback}></Button>
+      </div>
+      <Statistics feedback={feedback} />
+    </>
   );
 }
 
-const Header = ({ props }) => {
-  return <h1>{props}</h1>;
-};
+function Statistics({ feedback }) {
+  const { good, neutral, bad } = feedback;
+  const total = bad + neutral + good;
+  const avg = total > 0 ? (bad * -1 + neutral * 0 + good * 1) / total : 0;
+  const positive = total > 0 ? (good / total) * 100 : 0;
 
-const Content = ({ props }) => {
-  return props.map((element) => (
-    <Part
-      key={element.name}
-      part={element.name}
-      exercises={element.exercises}
-    />
-  ));
-};
-
-const Part = ({ part, exercises }) => {
   return (
-    <p>
-      {part} {exercises}
-    </p>
+    <div>
+      <h1>statistics</h1>
+      {total > 0 ? (
+        <table>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="total" value={total} />
+          <StatisticLine text="average" value={avg.toPrecision(4)} />
+          <StatisticLine
+            text="positive"
+            value={positive.toPrecision(4) + " %"}
+          />
+        </table>
+      ) : (
+        <p>No feedback given</p>
+      )}
+    </div>
   );
-};
-
-const Total = ({ props }) => {
+}
+function StatisticLine({ text, value, format = "default" }) {
   return (
-    <p>
-      {`Number of exercises ${props.reduce(
-        (sum, part) => sum + part.exercises,
-        0
-      )}`}
-    </p>
+    <tbody>
+      {format == "percent" ? (
+        <tr>
+          <td>{`${text} : `}</td>
+          <td>{`${value}  %`}</td>
+        </tr>
+      ) : (
+        <tr>
+          <td>{`${text} : `}</td>
+          <td>{`${value}`}</td>
+        </tr>
+      )}
+    </tbody>
   );
-};
+}
+
+function Button({ text, value, onClick }) {
+  return <button onClick={() => onClick(value)}>{text}</button>;
+}
